ae69a31f636a7c39095a9af991f9124efd275316
Kyronn <kmorgan24>
2019-03-04 23:52:38 -0800

    Test 2 passing

--- cComputeSize.h
+++ cComputeSize.h
@@ -13,34 +13,43 @@ class cComputeSize : public cVisitor
         m_offset = 0;
         m_highWater = 0;
     }
-    void Visit(cAstNode *node) { VisitAllChildren(node); }
-    void Visit(cAssignNode *node) { VisitAllChildren(node); }
-    void Visit(cBinaryExprNode *node) { VisitAllChildren(node); }
+
     void Visit(cBlockNode *node)
     {
+        int startH = m_highWater;
+        int startOff = m_offset;
+
+        m_highWater = m_offset;
         VisitAllChildren(node);
-        node->SetSize(m_highWater);
+
+        if (m_offset > m_highWater)
+        {
+            m_highWater = m_offset;
+        }
+
+        node->SetSize(m_highWater - startOff);
+
+        if (startH > m_highWater)
+        {
+            m_highWater = startH;
+        }
+
+        m_offset = startOff;
     }
     void Visit(cDeclNode *node) { VisitAllChildren(node); }
     void Visit(cDeclsNode *node)
     {
+        int startOff = m_offset;
         VisitAllChildren(node);
-        node->SetSize(m_offset);
+        node->SetSize(m_offset - startOff);
     }
-    void Visit(cExprNode *node) { VisitAllChildren(node); }
-    void Visit(cFloatExprNode *node) { VisitAllChildren(node); }
+
     void Visit(cFuncDeclNode *node) { VisitAllChildren(node); }
-    void Visit(cFuncExprNode *node) { VisitAllChildren(node); }
-    void Visit(cIfNode *node) { VisitAllChildren(node); }
-    void Visit(cIntExprNode *node) { VisitAllChildren(node); }
-    void Visit(cOpNode *node) { VisitAllChildren(node); }
+
     void Visit(cParamListNode *node) { VisitAllChildren(node); }
-    void Visit(cPrintNode *node) { VisitAllChildren(node); }
-    void Visit(cReturnNode *node) { VisitAllChildren(node); }
-    void Visit(cStmtNode *node) { VisitAllChildren(node); }
-    void Visit(cStmtsNode *node) { VisitAllChildren(node); }
+
     void Visit(cStructDeclNode *node) { VisitAllChildren(node); }
-    void Visit(cSymbol *node) { VisitAllChildren(node); }
+
     void Visit(cVarDeclNode *node)
     {
         while (m_offset % 4 != 0)
@@ -53,12 +62,34 @@ class cComputeSize : public cVisitor
         if (m_offset > m_highWater)
         {
             m_highWater = m_offset;
+            // while (m_highWater % 4 != 0)
+            // {
+            //     ++m_highWater;
+            // }
         }
     }
-    void Visit(cVarExprNode *node) { VisitAllChildren(node); }
-    void Visit(cWhileNode *node) { VisitAllChildren(node); }
+
     virtual void VisitAllNodes(cAstNode *node)
     {
         VisitAllChildren(node);
     }
+
+    // dont need?
+
+    void Visit(cSymbol *node) { VisitAllChildren(node); }
+    void Visit(cPrintNode *node) { VisitAllChildren(node); }
+    void Visit(cReturnNode *node) { VisitAllChildren(node); }
+    void Visit(cStmtNode *node) { VisitAllChildren(node); }
+    void Visit(cStmtsNode *node) { VisitAllChildren(node); }
+    void Visit(cFuncExprNode *node) { VisitAllChildren(node); }
+    void Visit(cIfNode *node) { VisitAllChildren(node); }
+    void Visit(cIntExprNode *node) { VisitAllChildren(node); }
+    void Visit(cOpNode *node) { VisitAllChildren(node); }
+    void Visit(cExprNode *node) { VisitAllChildren(node); }
+    void Visit(cFloatExprNode *node) { VisitAllChildren(node); }
+    void Visit(cAstNode *node) { VisitAllChildren(node); }
+    void Visit(cAssignNode *node) { VisitAllChildren(node); }
+    void Visit(cBinaryExprNode *node) { VisitAllChildren(node); }
+    void Visit(cVarExprNode *node) { VisitAllChildren(node); }
+    void Visit(cWhileNode *node) { VisitAllChildren(node); }
 };
