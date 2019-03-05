281098cd680a1bbedf1fd8fba0a51729c83c137a
Kyronn <kmorgan24>
2019-03-05 14:20:33 -0800

    Added  cParamsnode visitor

--- cVisitor.cpp
+++ cVisitor.cpp
@@ -19,33 +19,34 @@ void cVisitor::PostVisitAllNodes(cAstNode *node)
 void cVisitor::VisitAllChildren(cAstNode *node)
 {
     cAstNode::iterator it;
-    for (it=node->FirstChild(); it!=node->LastChild(); it++)
+    for (it = node->FirstChild(); it != node->LastChild(); it++)
     {
-        if ((*it) != nullptr) (*it)->Visit(this);
+        if ((*it) != nullptr)
+            (*it)->Visit(this);
     }
 }
 
-void cVisitor::Visit(cAstNode *node)          { VisitAllChildren(node); }
-void cVisitor::Visit(cAssignNode *node)       { VisitAllChildren(node); }
-void cVisitor::Visit(cBinaryExprNode *node)   { VisitAllChildren(node); }
-void cVisitor::Visit(cBlockNode *node)        { VisitAllChildren(node); }
-void cVisitor::Visit(cDeclNode *node)         { VisitAllChildren(node); }
-void cVisitor::Visit(cDeclsNode *node)        { VisitAllChildren(node); }
-void cVisitor::Visit(cExprNode *node)         { VisitAllChildren(node); }
-void cVisitor::Visit(cFloatExprNode *node)    { VisitAllChildren(node); }
-void cVisitor::Visit(cFuncDeclNode *node)     { VisitAllChildren(node); }
-void cVisitor::Visit(cFuncExprNode *node)     { VisitAllChildren(node); }
-void cVisitor::Visit(cIfNode *node)           { VisitAllChildren(node); }
-void cVisitor::Visit(cIntExprNode *node)      { VisitAllChildren(node); }
-void cVisitor::Visit(cOpNode *node)           { VisitAllChildren(node); }
-void cVisitor::Visit(cParamListNode *node)    { VisitAllChildren(node); }
-void cVisitor::Visit(cPrintNode *node)        { VisitAllChildren(node); }
-void cVisitor::Visit(cReturnNode *node)       { VisitAllChildren(node); }
-void cVisitor::Visit(cStmtNode *node)         { VisitAllChildren(node); }
-void cVisitor::Visit(cStmtsNode *node)        { VisitAllChildren(node); }
-void cVisitor::Visit(cStructDeclNode *node)   { VisitAllChildren(node); }
-void cVisitor::Visit(cSymbol *node)           { VisitAllChildren(node); }
-void cVisitor::Visit(cVarDeclNode *node)      { VisitAllChildren(node); }
-void cVisitor::Visit(cVarExprNode *node)      { VisitAllChildren(node); }
-void cVisitor::Visit(cWhileNode *node)        { VisitAllChildren(node); }
-
+void cVisitor::Visit(cAstNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cAssignNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cBinaryExprNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cBlockNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cDeclNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cDeclsNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cExprNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cFloatExprNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cFuncDeclNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cFuncExprNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cIfNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cIntExprNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cOpNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cParamListNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cParamsNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cPrintNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cReturnNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cStmtNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cStmtsNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cStructDeclNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cSymbol *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cVarDeclNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cVarExprNode *node) { VisitAllChildren(node); }
+void cVisitor::Visit(cWhileNode *node) { VisitAllChildren(node); }
--- cVisitor.h
+++ cVisitor.h
@@ -15,6 +15,7 @@ class cIfNode;
 class cIntExprNode;
 class cOpNode;
 class cParamListNode;
+class cParamsNode;
 class cPrintNode;
 class cReturnNode;
 class cStmtNode;
@@ -27,36 +28,38 @@ class cWhileNode;
 
 class cVisitor
 {
-    public:
-        cVisitor() {}
+  public:
+    cVisitor() {}
 
-        virtual void VisitAllNodes(cAstNode *node) = 0;
+    virtual void VisitAllNodes(cAstNode *node) = 0;
 
-        virtual void Visit(cAstNode *node);
-        virtual void Visit(cAssignNode *node);
-        virtual void Visit(cBinaryExprNode *node);
-        virtual void Visit(cBlockNode *node);
-        virtual void Visit(cDeclNode *node);
-        virtual void Visit(cDeclsNode *node);
-        virtual void Visit(cExprNode *node);
-        virtual void Visit(cFloatExprNode *node);
-        virtual void Visit(cFuncDeclNode *node);
-        virtual void Visit(cFuncExprNode *node);
-        virtual void Visit(cIfNode *node);
-        virtual void Visit(cIntExprNode *node);
-        virtual void Visit(cOpNode *node);
-        virtual void Visit(cParamListNode *node);
-        virtual void Visit(cPrintNode *node);
-        virtual void Visit(cReturnNode *node);
-        virtual void Visit(cStmtNode *node);
-        virtual void Visit(cStmtsNode *node);
-        virtual void Visit(cStructDeclNode *node);
-        virtual void Visit(cSymbol *node);
-        virtual void Visit(cVarDeclNode *node);
-        virtual void Visit(cVarExprNode *node);
-        virtual void Visit(cWhileNode *node);
-    protected:
-        void PreVisitAllNodes(cAstNode *node);
-        void PostVisitAllNodes(cAstNode *node);
-        void VisitAllChildren(cAstNode *node);
+    virtual void Visit(cAstNode *node);
+    virtual void Visit(cAssignNode *node);
+    virtual void Visit(cBinaryExprNode *node);
+    virtual void Visit(cBlockNode *node);
+    virtual void Visit(cDeclNode *node);
+    virtual void Visit(cDeclsNode *node);
+    virtual void Visit(cExprNode *node);
+    virtual void Visit(cFloatExprNode *node);
+    virtual void Visit(cFuncDeclNode *node);
+    virtual void Visit(cFuncExprNode *node);
+    virtual void Visit(cIfNode *node);
+    virtual void Visit(cIntExprNode *node);
+    virtual void Visit(cOpNode *node);
+    virtual void Visit(cParamListNode *node);
+    virtual void Visit(cParamsNode *node);
+    virtual void Visit(cPrintNode *node);
+    virtual void Visit(cReturnNode *node);
+    virtual void Visit(cStmtNode *node);
+    virtual void Visit(cStmtsNode *node);
+    virtual void Visit(cStructDeclNode *node);
+    virtual void Visit(cSymbol *node);
+    virtual void Visit(cVarDeclNode *node);
+    virtual void Visit(cVarExprNode *node);
+    virtual void Visit(cWhileNode *node);
+
+  protected:
+    void PreVisitAllNodes(cAstNode *node);
+    void PostVisitAllNodes(cAstNode *node);
+    void VisitAllChildren(cAstNode *node);
 };
